10a0f51486791e98f320a53b019858eec8a51b1b
Michele Micunco <michele.micunco@accenture.com>
2026-04-19 12:45:22 +0200

    pulizia progetto

--- README.md
+++
@@ -1,1 +0,0 @@
-# my_fit_journal
--- apps/web/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  transpilePackages: ['@mfj/types', '@mfj/validators'],
-}
-
-export default nextConfig
